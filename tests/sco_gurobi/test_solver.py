b8498142358a7673d2090d1b88a6260029ccef78
NishanthJKumar <nishanth.kumar20@gmail.com>
2021-11-02 11:15:00 -0400

    fixes accidental commenting-out of gurobi solver tests

--- tests/sco_gurobi/test_solver.py
+++ tests/sco_gurobi/test_solver.py
@@ -87,33 +87,33 @@ def helper_test_prob(
 
 
 class TestSolver(unittest.TestCase):
-    # def test_prob0(self):
-    #     x0 = np.array([[1.0], [1.0]])
-    #     f = lambda x: np.array([[x[0, 0] ** 2 + x[1, 0] ** 2]])
-    #     g = lambda x: np.array([[3 - x[0, 0] - x[1, 0]]])
-    #     x_true = np.array([[1.5], [1.5]])
-    #     helper_test_prob(self, x0, x_true, f=f, g=g)
+    def test_prob0(self):
+        x0 = np.array([[1.0], [1.0]])
+        f = lambda x: np.array([[x[0, 0] ** 2 + x[1, 0] ** 2]])
+        g = lambda x: np.array([[3 - x[0, 0] - x[1, 0]]])
+        x_true = np.array([[1.5], [1.5]])
+        helper_test_prob(self, x0, x_true, f=f, g=g)
 
-    # def test_prob1(self):
-    #     x0 = np.array([[-2.0], [1.0]])
-    #     f = lambda x: np.array([[(x[1, 0] - x[0, 0] ** 2) ** 2 + (1 - x[0, 0]) ** 2]])
-    #     g = lambda x: np.array([[-1.5 - x[1, 0]]])
-    #     x_true = np.array([[1.0], [1.0]])
-    #     helper_test_prob(ut=self, x0=x0, x_true=x_true, f=f, g=g)
+    def test_prob1(self):
+        x0 = np.array([[-2.0], [1.0]])
+        f = lambda x: np.array([[(x[1, 0] - x[0, 0] ** 2) ** 2 + (1 - x[0, 0]) ** 2]])
+        g = lambda x: np.array([[-1.5 - x[1, 0]]])
+        x_true = np.array([[1.0], [1.0]])
+        helper_test_prob(ut=self, x0=x0, x_true=x_true, f=f, g=g)
 
-    # def test_prob2(self):
-    #     x0 = np.array([[10.0], [1.0]])
-    #     f = lambda x: np.array([[x[1, 0] + 1e-5 + (x[1, 0] - x[0, 0]) ** 2]])
-    #     g = lambda x: np.array([[-x[1, 0]]])
-    #     x_true = np.array([[0.0], [0.0]])
-    #     helper_test_prob(self, x0, x_true, f=f, g=g)
+    def test_prob2(self):
+        x0 = np.array([[10.0], [1.0]])
+        f = lambda x: np.array([[x[1, 0] + 1e-5 + (x[1, 0] - x[0, 0]) ** 2]])
+        g = lambda x: np.array([[-x[1, 0]]])
+        x_true = np.array([[0.0], [0.0]])
+        helper_test_prob(self, x0, x_true, f=f, g=g)
 
-    # def test_prob3(self):
-    #     x0 = np.array([[10.0], [1.0]])
-    #     f = lambda x: np.array([[(1 - x[0, 0]) ** 2]])
-    #     h = lambda x: np.array([[10 * (x[1, 0] - x[0, 0] ** 2)]])
-    #     x_true = np.array([[1.0], [1.0]])
-    #     helper_test_prob(self, x0, x_true, f=f, h=h)
+    def test_prob3(self):
+        x0 = np.array([[10.0], [1.0]])
+        f = lambda x: np.array([[(1 - x[0, 0]) ** 2]])
+        h = lambda x: np.array([[10 * (x[1, 0] - x[0, 0] ** 2)]])
+        x_true = np.array([[1.0], [1.0]])
+        helper_test_prob(self, x0, x_true, f=f, h=h)
 
     def test_prob4(self):
         x0 = np.array([[2.0], [2.0]])
@@ -122,47 +122,47 @@ class TestSolver(unittest.TestCase):
         x_true = np.array([[0.0], [np.sqrt(3)]])
         helper_test_prob(self, x0, x_true, f=f, h=h)
 
-    # def test_prob5(self):
-    #     x0 = np.array([[0.0], [0.0]])
-    #     angles = np.transpose(np.array(list(range(1, 7))) * 2 * np.pi / 6)
-    #     angles = angles.reshape((6, 1))
-    #     A_ineq = np.hstack((np.cos(angles), np.sin(angles)))
-    #     b_ineq = np.ones(angles.shape)
-    #     q = -np.array([[np.cos(np.pi / 6), np.sin(np.pi / 6)]])
-    #     x_true = np.array([[1], [np.tan(np.pi / 6)]])
-    #     helper_test_prob(self, x0, x_true, q=q, A_ineq=A_ineq, b_ineq=b_ineq)
+    def test_prob5(self):
+        x0 = np.array([[0.0], [0.0]])
+        angles = np.transpose(np.array(list(range(1, 7))) * 2 * np.pi / 6)
+        angles = angles.reshape((6, 1))
+        A_ineq = np.hstack((np.cos(angles), np.sin(angles)))
+        b_ineq = np.ones(angles.shape)
+        q = -np.array([[np.cos(np.pi / 6), np.sin(np.pi / 6)]])
+        x_true = np.array([[1], [np.tan(np.pi / 6)]])
+        helper_test_prob(self, x0, x_true, q=q, A_ineq=A_ineq, b_ineq=b_ineq)
 
-    # def test_prob6(self):
-    #     x0 = np.array([[0.0], [0.0]])
-    #     angles = np.transpose(np.array(list(range(1, 7))) * 2 * np.pi / 6)
-    #     angles = angles.reshape((6, 1))
-    #     Q = 0.1 * np.identity(2)
-    #     A_ineq = np.hstack((np.cos(angles), np.sin(angles)))
-    #     b_ineq = np.ones(angles.shape)
-    #     q = -np.array([[np.cos(np.pi / 6), np.sin(np.pi / 6)]])
-    #     g = lambda x: 0.01 * (A_ineq.dot(x) - b_ineq)
-    #     x_true = np.transpose(np.array([[1, np.tan(np.pi / 6)]]))
-    #     helper_test_prob(self, x0, x_true, Q=Q, q=q, g=g)
+    def test_prob6(self):
+        x0 = np.array([[0.0], [0.0]])
+        angles = np.transpose(np.array(list(range(1, 7))) * 2 * np.pi / 6)
+        angles = angles.reshape((6, 1))
+        Q = 0.1 * np.identity(2)
+        A_ineq = np.hstack((np.cos(angles), np.sin(angles)))
+        b_ineq = np.ones(angles.shape)
+        q = -np.array([[np.cos(np.pi / 6), np.sin(np.pi / 6)]])
+        g = lambda x: 0.01 * (A_ineq.dot(x) - b_ineq)
+        x_true = np.transpose(np.array([[1, np.tan(np.pi / 6)]]))
+        helper_test_prob(self, x0, x_true, Q=Q, q=q, g=g)
 
-    # def test_prob7(self):
-    #     x0 = np.array([[0.0], [0.0]])
-    #     f = lambda x: np.array([[x[0, 0] ** 4 + x[1, 0] ** 4]])
-    #     g = lambda x: np.array([[3 - x[0, 0] - x[1, 0]]])
-    #     h = lambda x: np.array([[x[0, 0] - 2 * x[1, 0]]])
-    #     x_true = np.array([[2.0], [1.0]])
-    #     helper_test_prob(self, x0, x_true, f=f, g=g, h=h)
+    def test_prob7(self):
+        x0 = np.array([[0.0], [0.0]])
+        f = lambda x: np.array([[x[0, 0] ** 4 + x[1, 0] ** 4]])
+        g = lambda x: np.array([[3 - x[0, 0] - x[1, 0]]])
+        h = lambda x: np.array([[x[0, 0] - 2 * x[1, 0]]])
+        x_true = np.array([[2.0], [1.0]])
+        helper_test_prob(self, x0, x_true, f=f, g=g, h=h)
 
-    # def test_prob8(self):
-    #     x0 = np.array([[5.0], [5.0]])
-    #     g = lambda x: np.vstack(
-    #         (
-    #             x[0, 0] ** 2 + x[1, 0] ** 2 - 4,
-    #             -((x[0, 0] - 1) ** 2 + (x[1, 0] - 1) ** 2 - 0.25),
-    #             -((x[0, 0] + 1) ** 2 + (x[1, 0] - 1) ** 2 - 0.25),
-    #             -((x[0, 0]) ** 2 + 7 * (x[1, 0] + 1 - x[0, 0] ** 2 / 2) ** 2 - 0.8),
-    #         )
-    #     )
+    def test_prob8(self):
+        x0 = np.array([[5.0], [5.0]])
+        g = lambda x: np.vstack(
+            (
+                x[0, 0] ** 2 + x[1, 0] ** 2 - 4,
+                -((x[0, 0] - 1) ** 2 + (x[1, 0] - 1) ** 2 - 0.25),
+                -((x[0, 0] + 1) ** 2 + (x[1, 0] - 1) ** 2 - 0.25),
+                -((x[0, 0]) ** 2 + 7 * (x[1, 0] + 1 - x[0, 0] ** 2 / 2) ** 2 - 0.8),
+            )
+        )
 
-    #     Q = np.identity(2)
-    #     x_true = np.array([[0.0], [0.0]])
-    #     helper_test_prob(self, x0, x_true, g=g, Q=Q)
+        Q = np.identity(2)
+        x_true = np.array([[0.0], [0.0]])
+        helper_test_prob(self, x0, x_true, g=g, Q=Q)
